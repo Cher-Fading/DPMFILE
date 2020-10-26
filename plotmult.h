d0fd3c7c2b8aa56c5c7d415a890827c7dbaeda6a
Xiaoning Wang <xwang97@eic0101.rcf.bnl.gov>
2020-10-26 06:40:21 -0400

    commit

--- plotmult.h
+++ plotmult.h
@@ -183,9 +183,9 @@ plotmult::plotmult(TTree *tree) : fChain(0)
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("fort91.root");
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("~/dpmjet3-32BIT/fort91.root");
       if (!f || !f->IsOpen()) {
-         f = new TFile("fort91.root");
+         f = new TFile("~/dpmjet3-32BIT/fort91.root");
       }
       f->GetObject("EICTree",tree);
 

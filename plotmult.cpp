00adf139a0c7f4eb2f285c99b92922c744bb9dd2
Cher-Fading <xw31@illinois.edu>
2020-10-26 05:59:36 -0500

    commit

--- plotmult.cpp
+++ plotmult.cpp
@@ -1,8 +1,12 @@
 void plotmult(string filename){
     gSystem->AddIncludePath("/afs/rhic/eic/restructured/env/pro/lib/");
   gSystem->Load("/afs/rhic/eic/restructured/env/pro/lib/libeicsmear.so");
-  gInterpreter->AddIncludePath("/afs/rhic/eic/restructured/env/pro/lib/");
-  gInterpreter->Load("/afs/rhic/eic/restructured/env/pro/lib/libeicsmear.so");
+ #include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/EventBase.h"
+#include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/EventPythia.h"
+#include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/Particle.h"
+#include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/EventDpmjet.h"
+#include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/Forester.h"
+#include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/ForesterStatus.h.betterbutdifferent"
     TFile* f = TFile::Open(Form("%s.root",filename.c_str()));
     TTree* EICTree = (TTree*)f->Get("EICTree");
     

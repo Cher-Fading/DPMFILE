11afb25ab4fc030feffbd4aff2cd4e676cdfec25
Cher-Fading <xw31@illinois.edu>
2020-10-28 07:11:46 -0500

    commit

--- dpmcondor.sh
+++ dpmcondor.sh
@@ -7,7 +7,7 @@ output=$((50 + $2))
 echo output$output
 TAB=$'\t'
 sed -i "s/^OUTPUT.*/OUTPUT           ${output}/" sphenix/user/xwang97/DPMJET/$1/$2/$1.inp
-sed -i "s/^FSEED.*/FSEED           $(($2-5))           $((10-$output))           $(($output-3))           $((13-$output))/" CondorResult/$1/$2/$1.inp
+sed -i "s/^FSEED.*/FSEED           $(($2-5))           $((10-$output))           $(($output-3))           $((13-$output))/" sphenix/user/xwang97/DPMJET/$1/$2/$1.inp
 ./dpmjet3.0-5F-new < sphenix/user/xwang97/DPMJET/$1/$2/$1.inp > sphenix/user/xwang97/DPMJET/$1/$2/log$1_$2.txt
 mv fort.${output} sphenix/user/xwang97/DPMJET/$1/$2/
 root -q 'BuildIt.C("'sphenix/user/xwang97/DPMJET/$1/$2/fort.${output}'","'sphenix/user/xwang97/DPMJET/$1/$2/'")'

199259ba36dbfc397b4a56e0bf44c9dad48f416d
Cher-Fading <xw31@illinois.edu>
2020-10-28 07:11:27 -0500

    commit

--- dpmcondor.sh
+++ dpmcondor.sh
@@ -2,7 +2,6 @@
 cd ~/dpmjet3-32BIT/
 mkdir -p sphenix/user/xwang97/DPMJET/$1
 mkdir -p sphenix/user/xwang97/DPMJET/$1/$2
-cd ..
 cp $1.inp sphenix/user/xwang97/DPMJET/$1/$2/
 output=$((50 + $2))
 echo output$output

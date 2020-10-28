cf73e8398794c926ae1555e718dff3bfa822f598
Cher-Fading <xw31@illinois.edu>
2020-10-28 05:27:35 -0500

    commit

---
+++ dpmcondor.sh
@@ -0,0 +1,13 @@
+cd CondorResult
+mkdir -p $1
+mkdir -p $1/$2
+cd ..
+cp $1.inp CondorResult/$1/$2/
+output=$((90 + $2))
+echo output$output
+TAB=$'\t'
+sed -i "s/^OUTPUT.*/OUTPUT           ${output}/" CondorResult/$1/$2/$1.inp
+./dpmjet3.0-5F-new < CondorResult/$1/$2/$1.inp > CondorResult/$1/$2/log$1_$2.txt
+mv fort.${output} CondorResult/$1/$2/
+root -q 'BuildIt.C("'CondorResult/$1/$2/fort.${output}'","'CondorResult/$1/$2/'")'
+mv CondorResult/$1/$2/fort.root CondorResult/$1/fort_$1_$2.root

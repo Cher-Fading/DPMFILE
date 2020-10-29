a68b89dd2b74c280203368128cacde0a6d1b9b7a
Xiaoning Wang <xwang97@eic0101.rcf.bnl.gov>
2020-10-29 02:33:13 -0400

    commit

--- dpmcondor.sh
+++ dpmcondor.sh
@@ -3,7 +3,7 @@ cd ~/dpmjet3-32BIT/
 mkdir -p /sphenix/user/xwang97/DPMJET/$1
 mkdir -p /sphenix/user/xwang97/DPMJET/$1/$2
 cp $1.inp /sphenix/user/xwang97/DPMJET/$1/$2/
-output=$((50 + $2))
+output=$((21 + $2))
 echo output$output
 TAB=$'\t'
 sed -i "s/^OUTPUT.*/OUTPUT           ${output}/" /sphenix/user/xwang97/DPMJET/$1/$2/$1.inp
--- read.cxx
+++ read.cxx
@@ -11,6 +11,7 @@
 #include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/EventBase.h"
 #include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/EventPythia.h"
 #include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/Particle.h"
+#include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/ParticleMC.h"
 #include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/EventDpmjet.h"
 #include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/Forester.h"
 #include "/afs/rhic/eic/restructured/env/pro/PACKAGES/eic-smear/include/eicsmear/erhic/ForesterStatus.h.betterbutdifferent"
@@ -30,13 +31,20 @@
 gROOT->LoadMacro("../atlasstyle-00-04-02/AtlasLabels.C");
 gROOT->LoadMacro("../atlasstyle-00-04-02/AtlasUtils.C");
 #endif
+const int Q2_max = 1e3;
+const float Q2_cut = 5.;
 
+const int listn = 16;
+const int partl[listn] = {11,13,22,81,111,
+130,211,310,313,321,
+323,411,421,431,2112,
+2212};
+const int charl[listn] = {-1,-1,0,0,0,
+0,-1,0,0,1,
+1,1,0,1,0,
+1};
 
-const int listn = 13;
-const int partl[listn] = {11,13,22,81,130,211,310,321,411,421,431,2112,2212};
-const int charl[listn] = {-1,-1,0,0,0,-1,0,1,1,0,1,0,1};
-
-void read(TString inFileNames, int nEvents ) {
+void read(TString inFileNames, int nEvents = 0) {
    
    // If the analysis solely uses TTree::Draw statements,
    // you don't need to load
@@ -56,6 +64,7 @@ void read(TString inFileNames, int nEvents ) {
    // We could add multiple files if we wanted.
    std::string inname = inFileNames.Data();
    std::string outname = inname.substr(inname.find_last_of("/",inname.find("/*.root")-1)+1,inname.rfind("/")-inname.find_last_of("/",inname.find("/*.root")-1)-1);
+   cout << outname << endl;
    tree.Add(inFileNames); // Wild cards are allowed e.g. tree.Add("*.root" );
 // tree.Add(/path/to/otherFileNames ); // etc... 
    
@@ -75,10 +84,10 @@ void read(TString inFileNames, int nEvents ) {
    
    // Now we can do some analysis...
    int q2_min = 1;
-   int q2_max = 20;
    Float_t q2_bins[31];   
   float initial = log(q2_min);
-  float incre = log(q2_max/q2_min)/30;
+  float incre = log(Q2_max/q2_min)/30;
+
   for (int i = 0; i < 31;i++)  {
       q2_bins[i] = TMath::Power(TMath::E(),initial); 
       initial = initial + incre;
@@ -101,34 +110,53 @@ void read(TString inFileNames, int nEvents ) {
   //TH1F* multP_2 = hotTH1F("Multi", "dN/N vs Track Multiplicity of Final State Particles", 30,0.5,30.5, "", "", kBlue, 0.3, 21, 1, false);//status = -1 & 1
    TH1F* Q2P = hotTH1F("Q2", "dN/N vs Q2",30,q2_bins, "", "",kRed, 0.3, 21, 1, true);
    // Loop over events:
+   if (nEvents == 0) nEvents = tree.GetEntries();
+   //cout << "nEvents: " << nEvents << endl;
    for(int i(0); i < nEvents; ++i ) {
       
       // Read the next entry from the tree.
       tree.GetEntry(i);
-      
+      //cout << "Q2: " << event->GetQ2() << endl;
+      int nParticles = event->GetNTracks();
+      if (i%10000 ==0) cout << "processing event " << i << " with particles " << nParticles << endl;
+      //if ((event->GetQ2())<Q2_cut) continue;
       // Fill the Q2 vs. x histogram:
       Q2P->Fill(event->GetQ2());
       
       // The event contains a vector (array) of particles.
-      int nParticles = event->GetNTracks();
-      cout << nParticles << endl;
+
+      //cout << nParticles << endl;
       // We now know the number of particles in the event, so loop over
       // the particles:
       int counter = 0;
       for(int j(0); j < nParticles; ++j ) {
-         const Particle* particle = event->GetTrack(j);
+         //const erhic::ParticleMCeA* particleeA = (erhic::ParticleMCeA*)event->GetTrack(j);
+	const Particle* particle = event->GetTrack(j);
          // Let's just select charged pions for this example:
          int pdg = particle->GetPdgCode();
 	 int ks = particle->GetStatus();
+//cout << "particle mass number: " << particleeA->massNum << endl;
+cout << "ks " << ks << endl;
+cout << "pdg " << pdg << endl;
 	 if (ks!=1 && ks!=-1 && ks!=1001) continue;
 	  bool charged = false;
+bool found = false;
       for (int k = 0; k < listn; k++){
-	if (partl[k]==pdg && charl[k]!=0) charged = true;
-	
+	if (partl[k]==abs(pdg)){
+found = true;
+	if (charl[k]!=0) charged = true;
+	}
+if (pdg==80000){
+charged = true;
+found = true;
+}
       }
+if (!found) cout << "not listed pdg id: " << pdg << endl;
 	if (!charged) continue;
          //if(abs(pdg) != 211 ) continue;
+
          double pt = particle->GetPt();
+cout << "pt " << pt << " GeV" << endl;
 	if (pt > 5 || pt < 0.1) continue;
          //ptHist.Fill(particle->GetPt());
          counter++;
@@ -145,7 +173,7 @@ void read(TString inFileNames, int nEvents ) {
    TCanvas* c0 = new TCanvas("c0","c0",500,500);
 
    TPad* thePad = (TPad*)c0->cd();
-   TH1F *h1 = thePad->DrawFrame(0,1,30,1e4);
+   TH1F *h1 = thePad->DrawFrame(0,1,30,nEvents);
    h1->SetXTitle("N_{ch}");
    h1->SetYTitle("dN/dN_{ch}");
    h1->SetTitle("Distribution of Track Multiplicity in Events");
@@ -154,24 +182,24 @@ void read(TString inFileNames, int nEvents ) {
    //multP_2->Write();
    //multP->Scale(1./multP->GetSumOfWeights());
    //multP_2->Scale(1./multP_2->GetSumOfWeights());
-   multP->Draw("SAME");
+   multP->Draw("SAME, LP");
    //multP_2->Draw("SAME");
    myMarkerText( 0.4, 0.85, kRed, 21, "Particle Status = 1,-1,1001", 1.2, 0.04);
    //myMarkerText( 0.6, 0.8, kBlue, 21, "Particle Abs(status) = 1", 1.2, 0.04);
-   myText(0.6,0.75,kBlack,"Q^{2} > 1 GeV",0.04);
+   myText(0.6,0.75,kBlack,Form("Q^{2} > %.1f GeV",Q2_cut),0.04);
    myText(0.6,0.7,kBlack,"0.1 GeV < p_{T}^{trk} < 5 GeV",0.04);
    c0->SetLogy();
    c0->SaveAs(Form("%s_mult.pdf",outname.c_str()));
 
    
-   h1 = thePad->DrawFrame(1,1,20,1e4);
+   h1 = thePad->DrawFrame(1,1,Q2_max,nEvents);
    h1->SetXTitle("Q^{2} (GeV^{2})");
    h1->SetYTitle("dN/dQ^{2} (GeV^{2})");
    h1->SetTitle("Distribution of Q^{2} in Events");
    Q2P->Write();
    //Q2P->Scale(1./Q2P->GetSumOfWeights());
    Q2P->Draw("SAME");
-//c0->SetLogx();
+   //c0->SetLogx();
    c0->SaveAs(Form("%s_q2.pdf",outname.c_str()));
    
    fout->Close();

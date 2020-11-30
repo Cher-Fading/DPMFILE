6b4f62d1ad1d1165492f31082750b010691c91d1
Xiaoning Wang <xwang97@eic0101.rcf.bnl.gov>
2020-11-29 23:56:25 -0500

    commit

--- listfiles.sh
+++ listfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#$1 jobname $2 job batch $3 job number $4 batch mode (true) single job mode $5 event number
-if [ $4 == "true" ]; then
-ls /sphenix/user/xwang97/DPMJET/$1_$2/fort*.root > "/sphenix/user/xwang97/DPMJET/$1_$2/$1_$2_files.txt"
+#$1 jobname $2 job batch $3 batch mode (true) single job mode $4 event number
+if [ $3 == "true" ]; then
+ls /sphenix/user/xwang97/DPMJET/$1_$2/fort*$4.root > "/sphenix/user/xwang97/DPMJET/$1_$2/$1_$2_files.txt"
 else
-ls /sphenix/user/xwang97/DPMJET/$1/fort*.root > "/sphenix/user/xwang97/DPMJET/$1/$1_files.txt"
+ls /sphenix/user/xwang97/DPMJET/$1/fort*$4.root > "/sphenix/user/xwang97/DPMJET/$1/$1_files.txt"
 fi

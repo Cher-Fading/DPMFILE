9c94c470e473ffc70d0dba7d5a368594f3d12d8b
Cher-Fading <xw31@illinois.edu>
2020-11-29 23:54:21 -0600

    commit

--- readcondor.sh
+++ readcondor.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-#$1 jobname $2 job batch $3 job number $4 batch mode (true) single job mode $5 event number
-if [ $4 == "true" ]; then
-input="/sphenix/user/xwang97/DPMJET/$1_$2/$1_$2_files.txt"
-else
-input = "/sphenix/user/xwang97/DPMJET/$1/$1_files.txt"
-fi
+#$1 jobname $2 job number $3 event number
+input="/sphenix/user/xwang97/DPMJET/$1/$1_files.txt"
 
 #indexline=$1
 linenumber=0
 while IFS= read -r line; do
-	if [ $3 -eq $linenumber ]; then
-		if [ "$4" == "true" ]; then
-			filename=/sphenix/user/xwang97/DPMJET/$1_$2/fort_$1_$2_$3_$5.root
-		else
-
-			filename=/sphenix/user/xwang97/DPMJET/$1/fort_$1_$3_$5.root
-		fi
-echo $filename
-		root -b -q -l 'read_condor.cxx("'$filename'")'
-		sleep 2
-	fi
-	linenumber=$((linenumber + 1))
+    if [ $2 -eq $linenumber ]; then
+        filename=/sphenix/user/xwang97/DPMJET/$1/fort_$1_$2_$3.root
+        echo $filename
+        root -b -q -l 'read_condor.cxx("'$filename'")'
+        sleep 2
+    fi
+    linenumber=$((linenumber + 1))
 done <"$input"
--- readcondor_mult.sh
+++ readcondor_mult.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-
-#./dpmcondor_mult.sh ep_HERA4 4000 1E4 200
+#./readcondor_mult.sh ep_HERA4 1E4 40
+#$1 jobname $2 event number $3 number of batches
 cd /sphenix/u/xwang97/DPMFILE
-C=40
+input="/sphenix/user/xwang97/DPMJET/$1_$2batch_nof.txt"
 #use a different condorjob for each batch of $400 simulations
-for ((i=1;i<=C;i++))
-do
+for ((i = 1; i <= $3; i++)); do
+    linenumber=1
+    while IFS= read -r line; do
+        if [ $i -eq $linenumber ]; then
+            num=$line
+        fi
+        linenumber=$((linenumber + 1))
+    done <"$input"
 
-cp /sphenix/u/xwang97/DPMFILE/readcondor.sh /sphenix/u/xwang97/DPMFILE/readcondor$1_$i.sh
-#cat /sphenix/u/xwang97/DPMFILE/dpmcondor$1$i.sh
-chmod +x /sphenix/u/xwang97/DPMFILE/readcondor$1_$i.sh
-cp /sphenix/u/xwang97/DPMFILE/readCondor.condor /sphenix/u/xwang97/DPMFILE/readCondor$1_$i.condor
-sed -i "s@^Executable.*@Executable   = /sphenix/u/xwang97/DPMFILE/readcondor${1}_${i}\.\sh@" /sphenix/u/xwang97/DPMFILE/readCondor$1_$i.condor
+    cp /sphenix/u/xwang97/DPMFILE/readcondor.sh /sphenix/u/xwang97/DPMFILE/readcondor$1_$i.sh
+    #cat /sphenix/u/xwang97/DPMFILE/dpmcondor$1$i.sh
+    chmod +x /sphenix/u/xwang97/DPMFILE/readcondor$1_$i.sh
+    cp /sphenix/u/xwang97/DPMFILE/readCondor.condor /sphenix/u/xwang97/DPMFILE/readCondor$1_$i.condor
+    sed -i "s@^Executable.*@Executable   = /sphenix/u/xwang97/DPMFILE/readcondor${1}_${i}\.\sh@" /sphenix/u/xwang97/DPMFILE/readCondor$1_$i.condor
+    sed -i "s/^Queue.*/Queue ${num}/" /sphenix/u/xwang97/DPMFILE/readCondor$1_$i.condor
+    sed -i "s/^Arguments.*/Arguments       = ${1}_${i} \$(Process) ${2}/" /sphenix/u/xwang97/DPMFILE/readCondor$1_$i.condor
+    #cat /sphenix/u/xwang97/DPMFILE/dpmCondor$1$i.condor
 
-sed -i "s/^Queue.*/Queue ${num}/" /sphenix/u/xwang97/DPMFILE/dpmCondor$1_$i.condor
-sed -i "s/^Arguments.*/Arguments       = ${1} ${i} \$(Process) true ${2}/" /sphenix/u/xwang97/DPMFILE/dpmCondor$1_$i.condor
-#cat /sphenix/u/xwang97/DPMFILE/dpmCondor$1$i.condor
-
-condor_submit /sphenix/u/xwang97/DPMFILE/readCondor$1_$i.condor
+    #condor_submit /sphenix/u/xwang97/DPMFILE/readCondor$1_$i.condor
 done

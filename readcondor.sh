780ae4d2d66fc9c333dbd69607b747c92f2b2b5e
Xiaoning Wang <xwang97@eic0101.rcf.bnl.gov>
2020-11-30 10:22:55 -0500

    commit

--- readcondor.sh
+++ readcondor.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #$1 jobname $2 job number $3 event number
-input="/sphenix/user/xwang97/DPMJET/$1/$1_files.txt"
+input="/sphenix/user/xwang97/DPMJET/${1}/${1}_${3}_files.txt"
 
 #indexline=$1
 linenumber=0
 while IFS= read -r line; do
     if [ $2 -eq $linenumber ]; then
-        filename=/sphenix/user/xwang97/DPMJET/$1/fort_$1_$2_$3.root
+        filename=/sphenix/user/xwang97/DPMJET/${1}/fort_${1}_${2}_${3}.root
         echo $filename
         root -b -q -l 'read_condor.cxx("'$filename'")'
         sleep 2

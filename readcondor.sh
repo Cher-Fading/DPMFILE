c52cd9fab667ca9507991b567517ef298640a4b8
Xiaoning Wang <xwang97@eic0101.rcf.bnl.gov>
2020-12-14 00:39:10 -0500

    commit

--- readcondor.sh
+++ readcondor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#$1 jobname $2 job number $3 event number
+#$1 jobname $2 job number $3 event number $4 cut string $5 q2 min $6 lepton momentum $7 maximum q2
 input="/sphenix/user/xwang97/DPMJET/${1}/${1}_${3}_files.txt"
 
 #indexline=$1
@@ -8,7 +8,7 @@ linenumber=0
 while IFS= read -r line; do
     if [ $2 -eq $linenumber ]; then
         echo $line
-        root -b -q -l 'read_condor.cxx("'$line'")'
+        root -b -q -l 'read_condor.cxx("'$line'",0,false,"'$4'",'$5','$6','$7')'
         sleep 2
     fi
     linenumber=$((linenumber + 1))
